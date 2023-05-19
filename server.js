2aa8542f5aaf6d9d3e439e39454497dd0f79f12d
Danny Schaub <dschaub2@illinois.edu>
2023-05-19 11:03:22 -0500

    nearly implemented cookies, need to try a few things

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
+const {initCookie, getCookieInfo, setCookie} = require("./cookies.js")
+const cookieParser = require('cookie-parser')
 const jwt = require('jsonwebtoken')
 const bcrypt = require("bcrypt")
 const PORT = 3000
 
+const baseURL = 'http://localhost:3000'
+
 var tempUsers = []  //store in a database to keep away from attackers
 var refreshTokens = []  //Implement cookies to keep away from attackers
+
+app.use(cookieParser())
 
 app.post('/create-user', async (req, res) => {
     try{
@@ -46,7 +52,6 @@ function authenticateToken(req, res, next){
 
 app.post('/login', async (req, res) => {
     try{
-        
         const user = tempUsers.find(user => user['username'] === req.body.username)
         if(user){
             try{
@@ -69,16 +74,104 @@ app.post('/login', async (req, res) => {
         const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '300s' })
         const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN)
         
-        refreshTokens.push(refreshToken)
-        res.json({message: "Successfully logged in.", accessToken: accessToken, refreshToken: refreshToken})
+        refreshTokens.push(refreshToken) //replace with cookies/database
 
+        
+        const today = new Date()
+        res.cookie("RefreshToken", refreshToken, {
+            expires: today.setFullYear(today.getFullYear + 3),
+            httpOnly: true,
+            sameSite: 'lax',
+            secure: true
+        })
+        
+        const tenMinutes = new Date(Date.now() + 600*1000)
+        res.cookie("AccessToken", accessToken, {
+            expires: tenMinutes,
+            httpOnly: true,
+            sameSite: 'lax',
+            secure: true
+        })
+        
+        res.json({message: "Successfully logged in.", accessToken: accessToken, refreshToken: refreshToken})
     } catch{
         res.status(401).send("Error occured while logging in.")
     }
 })
 
+app.get("/login", (req, res) => {
+    res.send("Log in Here\nCreate Account here")
+})
+
+//http://localhost:3000/readCookies
+app.get("/readCookies", (req, res) => {
+    const accessToken = req.cookies['AccessToken']
+    const refreshToken = req.cookies['RefreshToken']
+
+    res.send(`Value of 'my-cookie' cookie: ${accessToken}\n${refreshToken}`)
+})
+
+//http://localhost:3000/loginRequest?username=Danny&password=secret
+app.get("/loginRequest", (req, res) => {
+    fetch(baseURL + "/login", {
+        method: "POST", 
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            "username": req.query.username,
+            "password": req.query.password
+        })
+    }).then(resp => {
+        if (!resp.ok) {
+            throw new Error('Failed to Log in');
+        }
+        return resp.json()
+    }).then(data => {
+        console.log('User logged in successfully') 
+        console.log(data.cookie)
+        res.send(
+            "message:" + data.message +
+            "\naccessToken:" + data.accessToken +
+            "\nrefreshToken:" + data.refreshToken
+        )
+    }).catch(error => {
+        res.send('Failed to Log in')
+        console.error(error)
+    })
+})
+
+//http://localhost:3000/create-user?username=Danny&password=secret
+app.get('/create-user', (req, res) => {
+    fetch(baseURL + "/create-user", {
+        method: "POST", 
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            "username": req.query.username,
+            "password": req.query.password
+        })
+    }).then(resp => {
+        if (!resp.ok) {
+            throw new Error('Failed to create user');
+        }
+        return resp
+    }).then(data => {
+        console.log('User created successfully')
+        res.send("Successfully created your account.")
+    }).catch(error => {
+        res.send('Failed to create user')
+        console.error(error)
+    })
+})
+
+app.use("/", (req, res) => {
+     res.send("Home Page")
+})
+
+
+
 app.post("/refreshToken", (req, res) => {
-    const refreshToken = req.body.refreshToken
+    const arr = document.cookie.split("; ")
+    const refreshToken = arr["RefreshToken"]
+    //const refreshToken = req.body.refreshToken
 
     if (refreshToken == null) 
         return res.status(401).send("Missing Refresh Token")
@@ -99,6 +192,11 @@ app.use('/logout', (req, res) => {
     res.sendStatus(204)
 })
 
+app.get('/cookies', (req, res) => {
+    res.json(getCookieInfo("AccessToken"), getCookieInfo("RefreshToken"), getCookieInfo("notValid"))
+})
+
 app.listen(PORT,  () => {
     console.log('> Ready on http://localhost:3000')
 })
+
